5b17c5f43cf97f92bfb3c9823178850e7598fe92
Jason Lee <jasonlee@hedra.com>
2025-07-24 15:05:23 -0400

    water testing log

--- @jsb188:app/utils/organization.ts
+++ @jsb188:app/utils/organization.ts
@@ -1,59 +1,73 @@
-import { OrganizationRelGQLData, OrganizationRelData } from "../types/organization.d";
+import { OrganizationRelData, OrganizationRelGQLData } from '../types/organization.d';
+
+const PERMISSION_TO_INT = {
+  NONE: 0,
+  READ: 1,
+  WRITE: 2,
+  MANAGE: 3,
+};
+
+const INT_TO_PERMISSION = Object.keys(PERMISSION_TO_INT);
 
 /**
  * Get deefault permissions by role
  */
 
 export function getDefaultPermissionsByRole(orgRel: OrganizationRelGQLData | OrganizationRelData) {
+	const role = orgRel?.role || 'MEMBER';
+  const acl: Record<string, any> = { ...orgRel?.acl };
 
-  const role = orgRel?.role || 'MEMBER';
-  const acl = orgRel?.acl || {};
-
-  // Also keep this synced with [resolvers/organizationTypeResolvers.ts]
-
-  // 0 = "NONE"
-  // 1 = "READ"
-  // 2 = "WRITE"
-  // 3 = "MANAGE"
-
-  switch (role) {
-    case 'ADMIN':
-    case 'OWNER':
-      return {
-        billing: 'MANAGE',
-        digests: acl.digests || 'MANAGE', // 2/3 = receive digests, 1 = only see from web app
-        logs: 'MANAGE',
-        members: 'MANAGE',
-        finances: 'MANAGE',
-        products: 'MANAGE',
-        settings: 'MANAGE',
-        integrations: 'MANAGE',
-      };
-    case 'MANAGER':
-      return {
-        billing: acl.billing || 'READ',
-        digests: acl.digests || 'WRITE', // 2/3 = receive digests, 1 = only see from web app
-        logs: acl.logs || 'MANAGE',
-        members: acl.members || 'WRITE',
-        finances: acl.finances || 'READ',
-        products: acl.products || 'MANAGE',
-        settings: acl.settings || 'READ',
-        integrations: acl.integrations || 'NONE',
-      };
-    case 'MEMBER':
-    default:
+  for (const key in acl) {
+    if (typeof acl[key] === 'number') {
+      acl[key] = INT_TO_PERMISSION[acl[key]] || 'NONE';
+    }
   }
 
-  return {
-    billing: acl.billing || 'NONE',
-    digests: acl.digests || 'NONE', // 2/3 = receive digests, 1 = only see from web app
-    logs: acl.logs || 'WRITE',
-    members: acl.members || 'READ',
-    finances: acl.finances || 'NONE',
-    products: acl.products || 'READ',
-    settings: acl.settings || 'READ',
-    integrations: acl.integrations || 'NONE',
-  };
+	// Also keep this synced with [resolvers/organizationTypeResolvers.ts]
+
+	// 0 = "NONE"
+	// 1 = "READ"
+	// 2 = "WRITE"
+	// 3 = "MANAGE"
+
+	switch (role) {
+		case 'ADMIN':
+		case 'OWNER':
+			return {
+				billing: 'MANAGE',
+				digests: acl.digests || 'MANAGE', // 2/3 = receive digests, 1 = only see from web app
+				logs: 'MANAGE',
+				members: 'MANAGE',
+				finances: 'MANAGE',
+				products: 'MANAGE',
+				settings: 'MANAGE',
+				integrations: 'MANAGE',
+			};
+		case 'MANAGER':
+			return {
+				billing: acl.billing || 'READ',
+				digests: acl.digests || 'WRITE', // 2/3 = receive digests, 1 = only see from web app
+				logs: acl.logs || 'MANAGE',
+				members: acl.members || 'WRITE',
+				finances: acl.finances || 'READ',
+				products: acl.products || 'MANAGE',
+				settings: acl.settings || 'READ',
+				integrations: acl.integrations || 'NONE',
+			};
+		case 'MEMBER':
+		default:
+	}
+
+	return {
+		billing: acl.billing || 'NONE',
+		digests: acl.digests || 'NONE', // 2/3 = receive digests, 1 = only see from web app
+		logs: acl.logs || 'WRITE',
+		members: acl.members || 'READ',
+		finances: acl.finances || 'NONE',
+		products: acl.products || 'READ',
+		settings: acl.settings || 'READ',
+		integrations: acl.integrations || 'NONE',
+	};
 }
 
 /**
@@ -63,25 +77,19 @@ export function getDefaultPermissionsByRole(orgRel: OrganizationRelGQLData | Org
 type ACLPermissionCheck = 'READ' | 'WRITE' | 'MANAGE';
 
 export function checkACLPermission(
-  orgRel: OrganizationRelGQLData | OrganizationRelData,
-  check: keyof ReturnType<typeof getDefaultPermissionsByRole>,
-  requiredPermission: ACLPermissionCheck
+	orgRel: OrganizationRelGQLData | OrganizationRelData,
+	check: keyof ReturnType<typeof getDefaultPermissionsByRole>,
+	requiredPermission: ACLPermissionCheck,
 ): boolean | null {
-  if (!orgRel) {
-    return null;
-  }
+	if (!orgRel) {
+		return null;
+	}
 
-  const acl = getDefaultPermissionsByRole(orgRel);
+	const acl = getDefaultPermissionsByRole(orgRel);
 
-  const permissionsToInt = {
-    READ: 1,
-    WRITE: 2,
-    MANAGE: 3,
-  };
+	const requiredInt = PERMISSION_TO_INT[requiredPermission] || 4; // If invalid, it will always return false
+	const permission = acl[check] as ACLPermissionCheck;
+	const acccountPermissionInt = PERMISSION_TO_INT[permission] || 0; // If invalid, it will always return false
 
-  const requiredInt = permissionsToInt[requiredPermission] || 4; // If invalid, it will always return false
-  const permission = acl[check] as ACLPermissionCheck;
-  const acccountPermissionInt = permissionsToInt[permission] || 0; // If invalid, it will always return false
-
-  return acccountPermissionInt >= requiredInt;
+	return acccountPermissionInt >= requiredInt;
 }
